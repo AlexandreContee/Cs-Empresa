fd3ebcd70a536ecc3aad2d26db8c61f1af3d91e6
Alexandre Conte <alexandre.conte10@gmail.com>
2022-11-28 08:49:37 -0300

    Final exercise version

---
+++ Empresa/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Empresa
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto *= (porcentagem + 100) / 100;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
